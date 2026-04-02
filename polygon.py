a82b056055f77389a731875c119ed034e96855a9
Colton <coltosb@outlook.com>
2026-04-02 15:02:51 -0700

    Made it easier to create objects and improved output

--- polygon.py
+++ polygon.py
@@ -6,6 +6,8 @@ class Point:
             self.x = x
         if y is not None:
             self.y = y
+    def __str__(self):
+        return f"({self.x},{self.y})"
 
 class Polygon:
     def __init__(self,points):
@@ -18,12 +20,16 @@ class Polygon:
         elif isinstance(points, tuple):
             self.points = points
 
-    def DoesPointIntersect(self,point):
+    def DoesPointIntersect(self,point=None,x=None,y=None):
         if not self.points:
             return None
 
+        if x is None or y is None:
+            if point is None:
+                return None
+            x, y = point.x, point.y
+
         num_vertices = len(self.points)
-        x, y = point.x, point.y
         inside = False
 
         # Store the first point in the polygon and initialize the second point
@@ -53,6 +59,8 @@ class Polygon:
 
         # Return the value of the inside flag
         return inside
+    def __str__(self):
+        return " -> ".join([str(x) for x in self.points])
 
 if __name__ == "__main__":
     polygon = Polygon([Point(10,10),Point(10,100),Point(100,100),Point(100,10)])
