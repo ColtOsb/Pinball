c34f2dd622ec231e5c396e5f54254e0caa64bd53
banarama62 <gill2092@vandals.uidaho.edu>
2026-04-25 22:48:24 -0700

    Can execute pytorch test from main script

--- __main__.py
+++ __main__.py
@@ -1,8 +1,24 @@
 import parse_args
 
 def Main():
-    print("Hello")
     args = parse_args.Setup()
+    mode = args["command"]
+    if not mode:
+        print("Invalid mode selected.")
+        exit(1)
+
+    elif mode == "run":
+        pass
+
+    elif mode == "test-torch":
+        device_id = args.get("device_id")
+        
+        function_args = {"print_results": True}
+        if device_id is not None:
+            function_args |= {"device_id": device_id}
+
+        from environments.test_torch.run import TorchTest
+        TorchTest.Test(**function_args)
 
 
 if __name__ == "__main__":
