cc148891f5dd41de4510f6ce7e84bb4a9cfa861a
banarama62 <gill2092@vandals.uidaho.edu>
2026-04-25 23:46:37 -0700

    Changed individual test names to be double-underscore-prefixed

--- testcases/test_torch.py
+++ testcases/test_torch.py
@@ -2,27 +2,27 @@ from .testcase import Testcase
 class TorchTest (Testcase):
     torch = None
 
-    def TestImportTorch():
+    def __TestImportTorch():
         import torch
         TorchTest.torch = torch
         return None
 
-    def TestGPUAvailable():
+    def __TestGPUAvailable():
         return TorchTest.torch.cuda.is_available()
 
-    def TestDeviceName(device_id):
+    def __TestDeviceName(device_id):
         return f"Device[{device_id}]: {TorchTest.torch.cuda.get_device_name(device_id)}"
         
-    def TestTorchEnv():
+    def __TestTorchEnv():
         return TorchTest.torch.utils.collect_env.get_pretty_env_info()
 
     def Test(device_id=0, print_results=False, return_results=True, output_level=0):
 
         tests = [
-                 ("Importing torch",TorchTest.TestImportTorch,[]),
-                 ("CUDA/GPU Available",TorchTest.TestGPUAvailable,[]),
-                 ("Device Name",TorchTest.TestDeviceName,[device_id]),
-                 ("Torch Env",TorchTest.TestTorchEnv,[])
+                 ("Importing torch",TorchTest.__TestImportTorch,[]),
+                 ("CUDA/GPU Available",TorchTest.__TestGPUAvailable,[]),
+                 ("Device Name",TorchTest.__TestDeviceName,[device_id]),
+                 ("Torch Env",TorchTest.__TestTorchEnv,[])
                  ]
 
         return Testcase.RunTests(tests,print_results,return_results,output_level)
